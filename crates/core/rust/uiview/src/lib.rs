09344c0aa0d5674796c06b968f26b2aa9c969ce8
Matt Marshall <mmarshall@savvifi.com>
2026-09-21 11:41:58 -0400

    fix(uiview): restore cargo proto namespace

--- crates/core/rust/uiview/src/lib.rs
+++ crates/core/rust/uiview/src/lib.rs
@@ -53,7 +53,7 @@ pub mod proto {
 #[cfg(not(bazel_proto))]
 pub mod proto {
     include!(concat!(env!("OUT_DIR"), "/meridian.ui.v1.rs"));
-    pub use self::meridian::ui::v1::*;
+    pub use meridian::ui::v1::*;
 }
 
 mod conversation;
